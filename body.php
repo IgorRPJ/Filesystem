003a943419506213920712af42965208a7ec9773
IgorRPJ <133509011+IgorRPJ@users.noreply.github.com>
2024-11-20 19:46:06 -0300

    Add files via upload

---
+++ body.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        nav{
+            display: flex;
+            justify-content: space-around;
+        }
+        nav > a{
+            color: white;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <nav>
+        <a href="contact.php">Contact</a>
+        <a href="about.php">About</a>
+        <a href="home.php">Home</a>
+    </nav>
+    
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        *{
+            color: white;
+        }
+        body{
+            background-color: rgb(0, 0, 80);
+        }
+        header{
+            background-color: black;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <header align='center'> este é o cabeçalho</header>
+    <?php include 'body.php'; ?>
+</body>
+</html>
